0674b026823612e3ccff3e900aa87f4aecbb43c7
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-10-09 22:34:03 +0200

    Begun on the client

--- client/main.go
+++ client/main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	"github.com/kanerix/chitty-chat/pb"
-	"github.com/spf13/cobra"
-)
-
-type Client struct {
-	pb.ChatClient
-}
-
-func (c *Client) Connect(user *pb.User) (*pb.Message, error) {
-	return nil, nil
-}
-
-var rootCmd = &cobra.Command{
-	Use:   "chitty-chat <username>",
-	Short: "A simple chat application using gRPC",
-	Long: `Chitty-chat is a chat service that allows users
-	to connect, send messages, and leave a chat room.
-		
-	Chitty-chat is built using gRPC and Protocol Buffers.`,
-}
+import "fmt"
 
 func main() {
-	user := flag.String("user", "Anonymous", "The username of the user to connect as.")
-	flag.Parse()
-	fmt.Println("Hello,", *user)
+	fmt.Println("Hello, world!")
 }
